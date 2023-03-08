93d629053765f63aafc880fc84b9535cd3733bf5
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-03-08 14:56:23 +0100

    FizzBuzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,2 +1,43 @@
-package org.lessons.java;public class FizzBuzz {
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        int infLimit = 1;
+        int supLimit = 1000;
+
+        int numbers = 0;
+
+        String fizzBuzz = "";
+
+        Scanner scan = new Scanner(System.in);
+
+        do {
+
+            System.out.println("Inserisci un numero tra " + infLimit + " e " + supLimit);
+            numbers = scan.nextInt();
+
+        } while(numbers < infLimit || numbers > supLimit);
+
+        for(int i = 1; i<=numbers; i++)
+        {
+            fizzBuzz = "Numero "+i;
+            if(i%3 == 0)
+            {
+                fizzBuzz+=" Fizz ";
+            }
+
+            if(i%5 == 0)
+            {
+
+                fizzBuzz+=" Buzz ";
+
+            }
+
+            System.out.println(fizzBuzz);
+
+        }
+    }
 }
--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,2 +1,41 @@
-package org.lessons.java;public class TrainTicket {
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class TrainTicket {
+
+    public static void main(String[] args) {
+
+        int kilometers = 0;
+        int age = 0;
+
+        double priceKm = 0.21;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci i chilometri da percorrere: ");
+        kilometers = scan.nextInt();
+        System.out.println("Inserisci l'età del passeggero: ");
+        age = scan.nextInt();
+
+        double ticketPrice = kilometers * priceKm;
+
+        if(age < 18)
+        {
+            ticketPrice*=0.8;
+            System.out.println("Il prezzo del biglietto è scontato del 20% e costa "+ticketPrice);
+        }
+        else if (age > 65) {
+
+            ticketPrice*=0.6;
+            System.out.println("Il prezzo del biglietto è scontato del 20% e costa "+ticketPrice);
+
+        } else {
+
+            System.out.println("Il prezzo del biglietto non è scontato e costa "+ticketPrice);
+
+        }
+
+    }
+
 }
